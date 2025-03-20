8bee8ae35f362904429e80ab7f9535e8459824c1
Shawn <iamshawnhartline@gmail.com>
2025-03-20 18:47:16 -0500

    class

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../shared/services/task.service';
 
 @Component({
   selector: 'app-task-form',
@@ -7,4 +8,19 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
-export class TaskFormComponent {}
+export class TaskFormComponent {
+  taskSevice = inject(TaskService)
+  tasks = this.taskSevice.tasks
+  newTaskTitle = signal<string>('')
+
+  addTaskHandler(title: string) {
+    
+    if (title) {
+      this.taskSevice.addTask(title)      
+    }else {
+      alert("You need to enter a task!")
+    }
+    this.newTaskTitle.set('');
+  }
+  
+}
